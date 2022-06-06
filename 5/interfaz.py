bc080936b0e839565b712fd9fd08c6c879e1c332
anto144 <104173968+anto144@users.noreply.github.com>
2022-06-06 03:46:34 -0300

    Add files via upload

---
+++ 5/interfaz.py
@@ -0,0 +1,12 @@
+from zope.interface import interface
+from zope.interface import implementer
+class Ielemento(interface):
+    def instertarElemento(elemento):
+      
+        pass
+    def agregarElemento(posicion,elemento):
+        #para agregar un elemento al final de una colección
+        pass
+    def mostrarElemento():
+       
+        pass
